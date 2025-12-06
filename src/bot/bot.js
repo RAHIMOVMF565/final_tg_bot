c85cdd727ca74b17560cac1d4ed417f7141a77e8
shoxruz <shoxruz@example.com>
2025-12-06 15:39:00 +0500

    botni rivojlantirdim

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,28 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+
+
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js";
+import onError from "./handlers/onError.js";
+config();
+
+export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+  const text = msg.text;
+
+  if (text == "/start") {
+    return onStart(msg);
+  }
+
+  if (text == "/profile") {
+    return onProfile(msg);
+  }
+
+  return onError(msg);
+});
+
 console.log("Bot ishga tushdi...");
